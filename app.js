bd2b1d1eff2b769f14b5652896ec13c107171c19
Zezinhodois <123456789+kaio.bg12@gmail.com>
2024-03-10 02:59:07 -0300

    adicionei logica js e esqueleto css e html

---
+++ app.js
@@ -0,0 +1,37 @@
+//criar as funcoes da operaçoes basicas
+//tres variaveis uma para cada operacao 
+// Duas variavel para os numeros e uma para o resultado do operador  
+//crie um funcao q receba dois valores pra executa a funcao 
+
+//adicao
+
+function Adicao(numero1, numero2) {
+    return numero1 + numero2
+}
+
+//subtracao
+
+function subtracao(numero1, numero2) {
+    return numero1 - numero2
+}
+
+//multiplicacao
+
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2
+}
+
+
+//divisao
+
+function divisao(numero1, numero2) {
+    return numero1 / numero2
+}
+console.log(divisao(2, 4))
+console.log(multiplicacao(2, 4))
+console.log(subtracao(2, 4))
+console.log(Adicao(2, 4))
+
+
+
+
