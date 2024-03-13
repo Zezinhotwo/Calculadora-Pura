c4eff186b90347aea3712c46c70f5ce5bceae74d
Zezinhodois <123456789+kaio.bg12@gmail.com>
2024-03-13 20:23:59 -0300

    coloquei a funcao somar e limpar-tela

--- app.js
+++ app.js
@@ -3,43 +3,65 @@
 // Duas variavel para os numeros e uma para o resultado do operador  
 //crie um funcao q receba dois valores pra executa a funcao 
 const num = document.querySelectorAll('.bnt-num');
-const operadores = document.querySelectorAll(".bnt-log")
+const operadores = document.querySelectorAll(".bnt-log");
 const tela = document.querySelector(".visor");
-const clear = document.getElementById("clear")
+const clear = document.getElementById("clear");
+const clear1 = document.getElementById("clear1");
+const result = document.getElementById("result");
 
-let i = 0;
 let aux = [];
+
 num.forEach((numero) => {
     numero.addEventListener("click", () => {
-        console.log(numero.value)
-        // aux.push(numero)
-        if (aux.length < 10) {
-            aux.push(numero.value)
-            tela.innerText = aux.join("")
-            console.log(aux)
-        } else if (aux.length >= 10) alert("capacidade maxima de '10' Numero ")
-        // console.log(i)
+        aux.push(numero.value);
+        tela.innerText = aux.join("");
+    });
+});
 
-    })
-})
-operadores.forEach((operandos) => {
-    operandos.addEventListener("click", () => {
-        aux.push(operandos.value)
-        tela.innerText =aux.join("")
-    })
-})
-clear.addEventListener("click",()=>{
-      tela.innerContent=aux.pop()
-      tela.innerText = aux.join("")
-})
+operadores.forEach((operador) => {
+    operador.addEventListener("click", () => {
+        aux.push(operador.value);
+        tela.innerText = aux.join("");
+    });
+});
+
+clear.addEventListener("click", () => {
+    aux.splice(0);
+    tela.innerText = "";
+});
+
+clear1.addEventListener("click", () => {
+    aux.pop();
+    tela.innerText = aux.join("");
+});
+
+result.addEventListener("click", () => {
+    let result = aux;
+    if (result.find(index => index == "+")) {
+        let resulM = result.filter(index => index != "+").map(index => Number(index))
+
+        console.log(Adicao(resulM))
+        tela.innerText = `${Adicao(resulM)}`;
+    }
+    else if (result.find(index => index == "-")) {
+        let resulS = result.filter(index => index != "-").map(index => Number(index))
+
+        console.log(subtracao(resulS))
+        tela.innerText = `${subtracao(resulS)}`;
+    }
 
 
-//adicao
+    function Adicao(numero1) {
 
-function Adicao(numero1, numero2) {
-    return numero1 + numero2
-}
+        return numero1.reduce((soma, indexAtual) => soma + indexAtual, 0)
+    }
+    function subtracao(numero1) {
+        
+        return numero1.map(index => Number(index) - Number(index))
+    }
 
+});
+//soma
 //subtracao
 
 function subtracao(numero1, numero2) {
